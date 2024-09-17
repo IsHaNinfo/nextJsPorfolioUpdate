7a3e27ec341025ea85c0fa969fb11b1a3fa1f483
ishaninfo <ishaninfo56@gmail.com>
2024-09-18 00:45:24 +0530

    new

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import Hero from "@/components/main/Hero"
+import Skils from "@/components/main/Skils"
+import Encryption from "@/components/main/Encryption";
+import Projects from "@/components/main/Project";
+
+export default function Home() {
+  return (
+    <div>
+      <main className="h-full w-full"> 
+        <div className="flex flex-col  gap-20">
+          <Hero/>
+          <Skils/>
+          <Encryption/>
+          <Projects/>
+        </div>
+      </main>
+    </div>
+  );
+}
