d8012d1bfdaba6ac45d058f43ad2cc7ae122d4a1
ishaninfo <ishaninfo56@gmail.com>
2024-09-18 09:08:32 +0530

    change projects

--- components/main/Project.tsx
+++ components/main/Project.tsx
@@ -8,24 +8,46 @@ const Projects = () => {
       id="projects"
     >
       <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
-        My Projects
+        Featured Works
       </h1>
-      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
+      <div className="h-full w-full grid grid-cols-1 md:grid-cols-3 gap-10 px-10">
         <ProjectCard
-          src="/NextWebsite.png"
-          title="Modern Next.js Portfolio"
+          src="/blackfamily.png"
+          title="Black Family Chat App"
+          role="Back End Developer"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
         />
         <ProjectCard
-          src="/CardImage.png"
-          title="Interactive Website Cards"
+          src="/jcgn.png"
+          title="JCGN Web App"
+          role="Full Stack Developer"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
         />
         <ProjectCard
-          src="/SpaceWebsite.png"
-          title="Space Themed Website"
+          src="/pm.png"
+          title="Project Management System HIT"
+          role="Full Stack Developer"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
         />
+         <ProjectCard
+          src="/wattcheck.png"
+          title="Watt Checker Mobile App"
+          role="Back End Developer"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+        <ProjectCard
+          src="/tangora.jpg"
+          title="Tangora Admin Panel"
+          role="Full Stack Developer"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+        <ProjectCard
+          src="/oijo.png"
+          title="Oi-Jo Car Rental Web Application "
+          role="Full Stack Developer"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+        {/* Add more cards here if necessary */}
       </div>
     </div>
   );
--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -4,10 +4,11 @@ import React from "react";
 interface Props {
   src: string;
   title: string;
+  role:string;
   description: string;
 }
 
-const ProjectCard = ({ src, title, description }: Props) => {
+const ProjectCard = ({ src, title,role, description }: Props) => {
   return (
     <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]">
       <Image
@@ -20,6 +21,7 @@ const ProjectCard = ({ src, title, description }: Props) => {
 
       <div className="relative p-4">
         <h1 className="text-2xl font-semibold text-white">{title}</h1>
+        <p className="mt-2 text-gray-300">Role : {role}</p>
         <p className="mt-2 text-gray-300">{description}</p>
       </div>
     </div>
