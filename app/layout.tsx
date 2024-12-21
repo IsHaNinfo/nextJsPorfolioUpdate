f643b2cce707aa8c9edadc713fe43d40c93294e0
ishaninfo <ishaninfo56@gmail.com>
2024-12-21 14:45:59 +0530

    new

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ const inter = Inter ({subsets :['latin']})
 export const metadata: Metadata = {
   title: "Ishan Nethmina ",
   icons: {
-    icon: "/NavLogo.png"
+    icon: "./NavLogo.png"
   },
 };
 
--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -13,6 +13,7 @@ import Image from "next/image";
 const HeroContent = () => {
   return (
     <motion.div
+    id="about-me"
       initial="hidden"
       animate="visible"
       className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]"
@@ -23,7 +24,7 @@ const HeroContent = () => {
           className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
         >
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
-          <h1 className="Welcome-text text-[13px]">
+          <h1  className="Welcome-text text-[13px]">
             Fullstack Developer 
           </h1>
         </motion.div>
@@ -32,7 +33,8 @@ const HeroContent = () => {
           variants={slideInFromLeft(0.5)}
           className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
         >
-          <span>
+          <span
+          >
             Ishan
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
               {" "}
